f294f51a90bb7f309a9183e52ebf7c2ad70eae59
Chris Torrella <christorrella@gmail.com>
2018-06-12 10:09:52 -0400

    Store ads in an array of obj and read using '.map' (Super Bonus Credit)

--- src/Clickbait.js
+++ src/Clickbait.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 
 class AdCreator extends React.Component {
+
   render() {
     return (
-      <div className="small-6 medium-3 columns other-article">
-        <a href="#">
-          <img src="https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif" alt="orc" />
-          <p>{this.props.name}</p>
-        </a>
-      </div>
+      <div className={this.props.item.className}>
+          <a href={this.props.item.type}>
+            <img src={this.props.item.img} alt={this.props.item.altText} />
+            <p>{this.props.item.title}</p>
+          </a>
+        </div>
     )
   }
 }
@@ -18,11 +19,35 @@ class Clickbait extends React.Component {
     super()
 
     this.state = {
-      acrobats: [
-        'Kendall',
-        'Stewart',
-        'Owen',
-        'Chris',
+      Arr: [
+        {
+          className: "small-6 medium-3 columns other-article",
+          img: 'https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif',
+          link: '#',
+          altText: 'orc',
+          title: 'Single Orcs in Indianapolis',
+        },
+        {
+          className: "small-6 medium-3 columns other-article",
+          img: 'https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg',
+          link: '#',
+          altText: 'mountain',
+          title: "You won't believe what's under this mountain",
+        },
+        {
+          className: "small-6 medium-3 columns other-article",
+          img: 'http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg',
+          link: '#',
+          altText: 'gold',
+          title: 'Mine 20% more gold with One Weird Trick',
+        },
+        {
+          className: "small-6 medium-3 columns other-article",
+          img: 'http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg',
+          link: '#',
+          altText: 'hobbit',
+          title: 'Surprise for Indiana Hobbits born before 1999',
+        },
       ],
     }
   }
@@ -31,7 +56,11 @@ class Clickbait extends React.Component {
     return (
       <div className="small-12 columns other-articles">
         <h2>From around the Realm</h2>
-        {this.state.acrobats.map(acrobat => <AdCreator name={acrobat} />)}
+
+        {this.state.Arr.map(
+          items => <AdCreator item={items} />
+        )}
+
       </div>
     )
   }
